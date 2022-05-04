7b16da5f25103a2b0b20c28b8ee5ff8ca61486c5
Eric <lieriec320@gmail.com>
2022-05-04 14:48:22 -0500

    Completed lab05 01 02

--- lab06/01-font-switcher/index.js
+++ lab06/01-font-switcher/index.js
@@ -1,12 +1,22 @@
+let pSize = 20;
+let header = 32;
 const makeBigger = () => {
-   alert('make bigger!');
+   // alert('make bigger!');
+   pSize += 4;
+   header += 4;
+   document.querySelector('p').style.fontSize = `${pSize}px`;
+   document.querySelector('h1').style.fontSize = `${header}px`;
 };
 
 const makeSmaller = () => {
-   alert('make smaller!');
+   // alert('make smaller!');
+   pSize -= 4;
+   header -= 4;
+   document.querySelector('p').style.fontSize = `${pSize}px`;
+   document.querySelector('h1').style.fontSize = `${header}px`;
 };
 
-/*
-document.querySelector(???).addEventListener('click', makeBigger);
-document.querySelector(???).addEventListener('click', makeSmaller);
-*/
+
+document.querySelector('#a1').addEventListener('click', makeBigger);
+document.querySelector('#a2').addEventListener('click', makeSmaller);
+
--- lab06/02-theme-switcher/index.js
+++ lab06/02-theme-switcher/index.js
@@ -5,3 +5,12 @@
     2. Modify the className property of the body tag 
        based on the button that was clicked.
 */
+
+const changeTheme = (theme) => {
+   document.querySelector('body').className = theme;
+};
+
+document.querySelector('#default').addEventListener('click', () => changeTheme(''));
+document.querySelector('#ocean').addEventListener('click', () => changeTheme('ocean'));
+document.querySelector('#desert').addEventListener('click', () => changeTheme('desert'));
+document.querySelector('#high-contrast').addEventListener('click', () => changeTheme('high-contrast'));
